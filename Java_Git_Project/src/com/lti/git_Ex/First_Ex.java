000f31428d845d707795a3c5f5a0588e7858410b
Amit <shanmukh.v.i@gmail.com>
2019-11-03 13:24:33 +0530

    Java Example

---
+++ Java_Git_Project/src/com/lti/git_Ex/First_Ex.java
@@ -0,0 +1,11 @@
+package com.lti.git_Ex;
+
+public class First_Ex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld");
+
+	}
+
+}
